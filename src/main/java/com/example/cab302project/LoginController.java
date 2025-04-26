89b2f6d49c06161f39381e4ec6305c4b268fa044
Alex Thompson <66576264+Alex78149@users.noreply.github.com>
2025-04-26 16:22:14 +1000

    now reads user input in text fields and switches between login and register

--- src/main/java/com/example/cab302project/LoginController.java
+++ src/main/java/com/example/cab302project/LoginController.java
@@ -1,24 +1,36 @@
 package com.example.cab302project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import jdk.jshell.spi.ExecutionControl;
 
 public class LoginController {
     @FXML
     private Label introText;
+    @FXML
     private Boolean isLogin = true;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button switchState;
 
     public final String loginText = "Welcome Back!";
     public final String registerText = "Welcome Aboard!";
 
 
     @FXML
-    protected void onLoginButtonClick() {
+    protected void onSwitchStateClick() {
         if (isLogin){
             introText.setText(loginText);
+            switchState.setText("Register Instead");
         }else{
             introText.setText(registerText);
+            switchState.setText("Login Instead");
         }
         isLogin = !isLogin;
 
@@ -27,5 +39,12 @@ public class LoginController {
     @FXML
     protected void onSubmitButtonClick() {
         System.out.println("Hello");
+        if (emailField.getText() != null){
+            String email = emailField.getText();
+        }
+        if (passwordField.getText() != null){
+            String password = passwordField.getText();
+        }
+
     }
 }
