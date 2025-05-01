0421cffdc85458c2efafe247ee2fc50bfd2cb086
Alvin Luu <n11380896@qut.edu.au>
2025-05-01 11:26:29 +1000

    Fixing login screen

--- src/main/java/com/example/cab302project/models/SqliteUserDAO.java
+++ src/main/java/com/example/cab302project/models/SqliteUserDAO.java
@@ -95,10 +95,10 @@ public class SqliteUserDAO implements IUserDAO {
     }
 
     @Override
-    public boolean validateUser(String username, String password) {
-        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+    public boolean validateUser(String email, String password) {
+        String query = "SELECT * FROM users WHERE email = ? AND password = ?";
         try (PreparedStatement stmt = connection.prepareStatement(query)) {
-            stmt.setString(1, username);
+            stmt.setString(1, email);
             stmt.setString(2, password);
             ResultSet rs = stmt.executeQuery();
 
